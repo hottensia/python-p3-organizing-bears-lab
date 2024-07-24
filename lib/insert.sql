356a551ae38c02e26a63d95d8644f5d5a84873c8
Hottensiah Wanjiku <w.hottensiah96@gmail.com>
2024-07-24 08:53:24 +0300

    first commit

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES 
+    ('Mr. Chocolate','10','F','White','temp','0'),
+    ('Rowdy', '5', 'M', 'Brown', 'Aggressive', '1'),
+    ('Tabitha', '8', 'F', 'Black', 'Calm', '1'),
+    ('Sergeant Brown', '12', 'M', 'Brown', 'Strict', '1'),
+    ('Melissa', '6', 'F', 'White', 'Friendly', '1'),
+    ('Grinch', '15', 'M', 'Green', 'Grumpy', '0'),
+    ('Wendy', '9', 'F', 'Blonde', 'Playful', '1'),
+    (NULL,"8","F","pink","temp","0");
